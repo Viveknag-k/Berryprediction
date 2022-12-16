6d7a2286b7b050e285123b9d64aa3f8d20068dd2
Vivek Nag Kanuri <97533913+Viveknag-k@users.noreply.github.com>
2022-12-16 19:39:57 +0530

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -2,16 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-#import joblib
-#model = joblib.load('linear_regrs.joblib')
-
 import pickle
 model=pickle.load(open("lr.pkl","rb"))
 st.header(" Blueberry yield Prediction")
 st.write("Input Parameters")
 def ip_features():
-    #[37.5,42.5,0.75,0.50]
-    #[0.75,0.50,0.25,1]
+  
     clonesize=st.number_input("clonesize")
     honeybee=st.number_input("honeybee")
     bumbles=st.number_input("bumbles")
@@ -29,12 +25,10 @@ st.write(df)
 
 DF = pd.read_csv("C:\\Users\\Vivek Nag Kanuri\\Downloads\\WildBlueberryPollinationSimulationData.csv")
 x = DF[['clonesize','honeybee','bumbles','andrena','osmia']]
-#x=DF(features)
+
 y= DF['yield']
 
 model.fit(x,y)
 
 pred = model.predict(df)
 st.write('yield',pred)
-
-
